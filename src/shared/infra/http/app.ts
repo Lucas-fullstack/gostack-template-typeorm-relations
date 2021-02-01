e9b31f9457010088f349ca9fc770f62923943b0b
Lucas Ferreira <lucasesportf@gmail.com>
2021-01-31 22:14:02 -0300

    desafio_09

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,44 @@
+import 'reflect-metadata';
+
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+
+import AppError from '@shared/errors/AppError';
+import createConnection from '@shared/infra/typeorm';
+import routes from './routes';
+
+import '@shared/container';
+
+createConnection();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  if (process.env.NODE_ENV === 'development') {
+    console.log(err);
+
+    return response.status(500).json({
+      type: 'error',
+      message: err.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
+export default app;
